01472cc55aaa1e439ea19fa005d198823c5245d0
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-03-17 00:02:10 -0400

    tailwing configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className="bg-red-500 text-3xl font-bold underline">
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
