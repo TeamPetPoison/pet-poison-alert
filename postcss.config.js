11e52a143812503dc4bfd17dc6d19fbf9713d076
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-03-19 14:00:35 +0800

    update postcss config to match docs

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  plugins: [
-    'postcss-import',
-    'tailwindcss',
-    'postcss-nested',
-    'postcss-preset-env',
-    'autoprefixer',
-  ],
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
