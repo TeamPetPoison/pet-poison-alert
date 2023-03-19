7b66e03d4731c7fc38abec7dc194cf98e1d2cfb5
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-03-19 14:01:11 +0800

    test tailwind colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,8 +12,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className="bg-background text-foreground">
+        {/* this can be removed, just a test */}
         <h1> Hello Team</h1>
+        {/* this can be removed, just a test */}
+        <button className="text-primary bg-foreground">Tailwind Test</button>
       </main>
     </>
   );
