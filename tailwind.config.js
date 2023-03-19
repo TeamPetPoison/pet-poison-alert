38b87909facc9a3b31422198e1acd70d9ddbcecd
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-03-19 13:42:54 +0800

    extend the colors tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      /**
+       * Colors are defined in global.css
+       * they are defined as CSS variables
+       * @see https://tailwindcss.com/docs/customizing-colors#using-css-variables
+       */
       colors: {
         primary: 'rgb(var(--color-primary) / var(--alpha-value))',
         foreground: 'rgb(var(--color-foreground) / var(--alpha-value))',
         background: 'rgb(var(--color-background) / var(--alpha-value))',
-        positive: 'green',
-        negative: 'red',
-        warning: 'yellow'
-      }
-    }
+        backgroundAlt: 'rgb(var(--color-background-alt) / var(--alpha-value))',
+        positive: 'rgb(var(--color-positive) / var(--alpha-value))',
+        negative: 'rgb(var(--color-negative) / var(--alpha-value))',
+        warning: 'rgb(var(--color-warning) / var(--alpha-value))',
+        info: 'rgb(var(--color-info) / var(--alpha-value))',
+      },
+    },
   },
   plugins: [],
-}
-
+};
