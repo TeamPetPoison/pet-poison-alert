a7e12aa49fef21255d18e3c0cb9a692d9df425e8
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-03-19 22:33:14 +0800

    width 100% for map

--- lib/Components/Map.jsx
+++ lib/Components/Map.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { useState } from 'react';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 const Map = () => {
   const [geoData, setGeoData] = useState({ lat: -8.7445, lng: 115.182 });
@@ -8,17 +8,13 @@ const Map = () => {
   const center = [geoData.lat, geoData.lng];
 
   return (
-    <MapContainer
-      center={center}
-      zoom={12}
-      style={{ height: "100vh", width: "100vw" }}
-    >
+    <MapContainer center={center} zoom={12} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
     </MapContainer>
   );
-}
+};
 
 export default Map;
