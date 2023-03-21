a1060511767d242b59e785588aa9a6910d513ffa
Rane Gray <ranegray830@gmail.com>
2023-03-21 23:41:57 +0100

    Finished first draft of BottomNav style

--- lib/Components/BottomNav.jsx
+++ lib/Components/BottomNav.jsx
@@ -8,25 +8,30 @@ import {
 
 const BottomNav = () => {
   return (
-    <div className="absolute bottom-0 z-[9999] w-full h-16 rounded-t-xl p-1 flex justify-between items-end bg-white">
-      <div>
-        <HomeIcon className="h-8 w-8" />
-        Home
+    <div className="absolute bottom-0 z-[9999] w-full h-16 text-slate-400 rounded-t-2xl p-1 grid grid-cols-5 justify-items-center items-center bg-white">
+      <div className="flex items-center justify-center flex-col">
+        <HomeIcon className="h-6 w-6" />
+        <div>Home</div>
       </div>
-      <div>
-        <MagnifyingGlassIcon className="h-8 w-8" />
-        Search
+      <div className="flex items-center justify-center flex-col">
+        <MagnifyingGlassIcon className="h-6 w-6" />
+        <div>Search</div>
       </div>
-      <div>
-        <PlusCircleIcon className="h-8 w-8" />
+      <div></div>
+      <div className="rounded-full fixed bottom-0 h-24 w-24 flex items-center justify-center bg-white">
+        <div className="rounded-full h-20 w-20 flex items-center justify-center bg-purple-500 ">
+          <div className="rounded-full h-16 w-16 flex items-center justify-center bg-slate-300">
+            <PlusCircleIcon className="h-6 w-6 text-purple-500" />
+          </div>
+        </div>
       </div>
-      <div>
-        <MapPinIcon className="h-8 w-8" />
-        Layers
+      <div className="flex items-center justify-center flex-col">
+        <MapPinIcon className="h-6 w-6" />
+        <div>Layers</div>
       </div>
-      <div>
-        <UserIcon className="h-8 w-8" />
-        Profile
+      <div className="flex items-center justify-center flex-col">
+        <UserIcon className="h-6 w-6" />
+        <div>Profile</div>
       </div>
     </div>
   );
