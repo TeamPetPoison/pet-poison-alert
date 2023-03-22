a5509dfa4ea168455bc90ab98e28ae3b2ddb3455
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-03-22 10:48:56 +0800

    move map to components folder

--- packages/client/lib/components/Map.jsx
+++ packages/client/lib/components/Map.jsx
@@ -8,11 +8,7 @@ const Map = () => {
   const center = [geoData.lat, geoData.lng];
 
   return (
-    <MapContainer
-      center={center}
-      zoom={12}
-      style={{ height: '100vh', width: '100%' }}
-    >
+    <MapContainer center={center} zoom={12} className="h-1 w-full bg-red-50">
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
