1c21725ac5d35f0375427da130cb711cb324c7c6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-03-23 14:21:56 +0800

    remove commented out code

--- lib/Components/Map.jsx
+++ lib/Components/Map.jsx
@@ -6,20 +6,6 @@ import { PlusIcon, FaceSmileIcon } from '@heroicons/react/24/solid';
 function Map() {
   const [geoData, setGeoData] = useState({ lat: -8.7445, lng: 115.182 });
 
-  // const center = L.latLng(geoData.lat, geoData.lng);
-
-  // const locationIconMarkup = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-  //   <path d="M10 0c-5.514 0-10 4.486-10 10 0 7.5 10 20 10 20s10-12.5 10-20c0-5.514-4.486-10-10-10zm0 14c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3-1.346 3-3 3z"/>
-  // </svg>`;
-
-  // const locationIconUrl = encodeURI(
-  //   "data:image/svg+xml;charset=utf-8," + locationIconMarkup
-  // );
-
-  // const locationIconSvg = new L.Icon({
-  //   iconUrl: locationIconUrl,
-  //   iconSize: [30, 30],
-  // });
   useEffect(() => {
     (async function init() {
       // delete Leaflet.Icon.Default.prototype._getIconUrl;
