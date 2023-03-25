27a9ad12659e0edc7a03174ab9a38dc81fa6f2f0
Rane Gray <ranegray830@gmail.com>
2023-03-25 13:00:18 +0100

    Removed relative positioning from main

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="relative bg-background text-foreground">
+      <main className="bg-background text-foreground">
         <MapWithNoSSR />
         <BottomNav />
       </main>
