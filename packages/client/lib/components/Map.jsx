5dd95be6caab05f04ccc1271e4ce59842767ead8
Rane Gray <ranegray830@gmail.com>
2023-03-28 15:44:27 +0200

    Working Geolocation implementation

--- packages/client/lib/components/Map.jsx
+++ packages/client/lib/components/Map.jsx
@@ -1,26 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import useStore from '../../store/store';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 
 const Map = () => {
-  // const [geoData, setGeoData] = useState({ lat: -8.7445, lng: 115.182 });
   const geoData = useStore((state) => state.geoData);
-  const setGeoData = useStore(state => state.setGeoData)
 
-  const center = [geoData.lat, geoData.lng];
+  const SetViewOnUserLocation = () => {
+    const map = useMap();
 
-  useEffect(() => {
-    let currentLocation = {lat: 0, lng: 0};
-    navigator.geolocation.getCurrentPosition((position) => {
-      currentLocation.lat = position.coords.latitude
-      currentLocation.lng = position.coords.longitude
-    });
-    setGeoData(currentLocation)
-  }, []);
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        map.setView(
+          { lat: position.coords.latitude, lng: position.coords.longitude },
+          12
+        );
+      });
+    }, [map]);
+
+    return null;
+  };
 
   return (
-    <MapContainer center={center} zoom={12} className="min-h-screen w-full">
+    <MapContainer center={geoData} zoom={12} className="min-h-screen w-full">
+      <SetViewOnUserLocation />
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
