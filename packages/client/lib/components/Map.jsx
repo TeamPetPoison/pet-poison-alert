72af75737c26411b2a0184e6982123393d4d4455
Rane Gray <ranegray830@gmail.com>
2023-03-28 14:45:15 +0200

    State management works, starting on map centering on current location

--- packages/client/lib/components/Map.jsx
+++ packages/client/lib/components/Map.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
-import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { useEffect, useState } from 'react';
+import useStore from '../../store/store';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 const Map = () => {
-  const [geoData, setGeoData] = useState({ lat: -8.7445, lng: 115.182 });
+  // const [geoData, setGeoData] = useState({ lat: -8.7445, lng: 115.182 });
+  const geoData = useStore((state) => state.geoData);
+  const setGeoData = useStore(state => state.setGeoData)
 
   const center = [geoData.lat, geoData.lng];
+
+  useEffect(() => {
+    let currentLocation = {lat: 0, lng: 0};
+    navigator.geolocation.getCurrentPosition((position) => {
+      currentLocation.lat = position.coords.latitude
+      currentLocation.lng = position.coords.longitude
+    });
+    setGeoData(currentLocation)
+  }, []);
 
   return (
     <MapContainer center={center} zoom={12} className="min-h-screen w-full">
--- packages/client/store/store.js
+++ packages/client/store/store.js
@@ -1,5 +1,8 @@
 import { create } from "zustand";
 
-const useStore = create((set) => ({
-    
+const useStore = create(set => ({
+    geoData: { lat: -8.7445, lng: 115.182 },
+    setGeoData: (currentLocation) => set(state => state.geoData = currentLocation)
 }))
+
+export default useStore;
