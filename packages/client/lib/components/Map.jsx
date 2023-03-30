4f127c937833f7d46c22adfbd4368ecf9b4950df
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-03-30 01:49:57 -0400

    merge changes with main branch

--- packages/client/lib/components/Map.jsx
+++ packages/client/lib/components/Map.jsx
@@ -1,10 +1,21 @@
 import { useEffect } from 'react';
 import useStore from '../../store/store';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
-
+import { MapContainer, TileLayer, Marker, ZoomControl,useMap } from 'react-leaflet';
+import Leaflet from 'leaflet';
 const SetViewOnUserLocation = () => {
   const map = useMap();
+
+  useEffect(() => {
+    (async function init() {
+      // delete Leaflet.Icon.Default.prototype._getIconUrl;
+      Leaflet.Icon.Default.mergeOptions({
+        iconRetinaUrl: 'leaflet/images/marker-icon-2x.png',
+        iconUrl: 'leaflet/images/marker-icon.png',
+        shadowUrl: 'leaflet/images/marker-shadow.png',
+      });
+    })();
+  }, []);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -28,12 +39,14 @@ const Map = () => {
   const geoData = useStore((state) => state.geoData);
 
   return (
-    <MapContainer center={geoData} zoom={12} className="min-h-screen w-full">
+    <MapContainer center={geoData} zoom={12} zoomControl={false} className="min-h-screen w-full">
       <SetViewOnUserLocation />
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={geoData} draggable />
+      <ZoomControl position="topright" />
     </MapContainer>
   );
 };
--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,3 +1,4 @@
+import 'leaflet/dist/leaflet.css';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
