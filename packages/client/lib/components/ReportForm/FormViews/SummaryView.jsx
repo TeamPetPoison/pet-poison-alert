e32eb4a1bf7ea5e32174ef5e971894e839b630b2
Rane Gray <ranegray830@gmail.com>
2023-03-31 13:18:30 +0200

    Refactored zustand actions and added global state and handlers to SummaryView

--- packages/client/lib/components/ReportForm/FormViews/SummaryView.jsx
+++ packages/client/lib/components/ReportForm/FormViews/SummaryView.jsx
@@ -1,4 +1,4 @@
-import useFormStore from "@/store/formStore";
+import useFormStore from '@/store/formStore';
 
 const SummaryView = () => {
   const { title } = useFormStore();
@@ -15,10 +15,12 @@ const SummaryView = () => {
         </label>
         <input
           type="text"
+          onChange={(e) => setTitle(e.target.value)}
           id="title"
           name="title"
+          value={title}
           placeholder="Provide a title for the report"
-          className="block w-full p-2 text-gray-400 border border-gray-300 rounded-lg bg-white sm:text-xs focus:ring-primary focus:border-primary shadow-md"
+          className="block w-full p-2 text-black border border-gray-300 rounded-lg bg-white focus:ring-primary focus:border-primary shadow-md"
         />
       </div>
       <h2 className="text-lg mt-4">Explain the incident</h2>
@@ -28,8 +30,10 @@ const SummaryView = () => {
       <textarea
         id="description"
         name="description"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
         rows={4}
-        className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-primary focus:primary shadow-md"
+        className="block p-2.5 w-full text-black bg-white rounded-lg border border-gray-300 focus:ring-primary focus:primary shadow-md"
         placeholder="Provide a description of your report"
       ></textarea>
     </>
--- packages/client/store/formStore.js
+++ packages/client/store/formStore.js
@@ -5,10 +5,10 @@ const useFormStore = create((set) => ({
   title: '',
   description: '',
   location: {},
-  setPhotos: (array) => set((state) => state.photos = array),
-  setTitle: (str) => set((state) => state.title = str),
-  setDescription: (str) => set((state) => state.description = str),
-  setLocation: (latLng) => set((state) => state.location = latLng)
+  setPhotos: (array) => set(() => ({photos: array})),
+  setTitle: (str) => set(() => ({title: str})),
+  setDescription: (str) => set(() => ({description: str})),
+  setLocation: (latLng) => set(() => ({location: latLng}))
 }));
 
 export default useFormStore;
