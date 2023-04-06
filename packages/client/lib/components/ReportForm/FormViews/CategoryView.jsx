83e9766b143e355a6713645ab32f18c77dd23eaa
Rane Gray <ranegray830@gmail.com>
2023-04-06 21:50:43 +0200

    Added onChange and checked statuses to radio buttons to persist through and track state between form steps

--- packages/client/lib/components/ReportForm/FormViews/CategoryView.jsx
+++ packages/client/lib/components/ReportForm/FormViews/CategoryView.jsx
@@ -7,32 +7,74 @@ const CategoryView = () => {
     <div>
       <h2 className="text-lg mt-4">Category of incident</h2>
       <div>
-          <input type="radio" name="lost-pet" id="lost-pet" />
-          <label htmlFor="lost-pet">Lost/Missing Pet</label>
+        <input
+          onChange={({ target }) => setCategory(target.name)}
+          checked={category === 'lost-pet'}
+          type="radio"
+          name="lost-pet"
+          id="lost-pet"
+        />
+        <label htmlFor="lost-pet">Lost/Missing Pet</label>
       </div>
       <div>
-          <input type="radio" name="found-pet" id="found-pet" />
-          <label htmlFor="found-pet">Found Pet</label>
+        <input
+          onChange={({ target }) => setCategory(target.name)}
+          checked={category === 'found-pet'}
+          type="radio"
+          name="found-pet"
+          id="found-pet"
+        />
+        <label htmlFor="found-pet">Found Pet</label>
       </div>
       <div>
-          <input type="radio" name="needs-medical" id="needs-medical" />
-          <label htmlFor="needs-medical">Needs Medical Treatment</label>
+        <input
+          onChange={({ target }) => setCategory(target.name)}
+          checked={category === 'needs-medical'}
+          type="radio"
+          name="needs-medical"
+          id="needs-medical"
+        />
+        <label htmlFor="needs-medical">Needs Medical Treatment</label>
       </div>
       <div>
-          <input type="radio" name="gave-medical" id="gave-medical" />
-          <label htmlFor="gave-medical">Gave/Received Medical Treatment</label>
+        <input
+          onChange={({ target }) => setCategory(target.name)}
+          checked={category === 'gave-medical'}
+          type="radio"
+          name="gave-medical"
+          id="gave-medical"
+        />
+        <label htmlFor="gave-medical">Gave/Received Medical Treatment</label>
       </div>
       <div>
-          <input type="radio" name="gave-food" id="gave-food" />
-          <label htmlFor="gave-food">Gave Food</label>
+        <input
+          onChange={({ target }) => setCategory(target.name)}
+          checked={category === 'gave-food'}
+          type="radio"
+          name="gave-food"
+          id="gave-food"
+        />
+        <label htmlFor="gave-food">Gave Food</label>
       </div>
       <div>
-          <input type="radio" name="poisoning" id="poisoning" />
-          <label htmlFor="poisoning">Poisoning</label>
+        <input
+          onChange={({ target }) => setCategory(target.name)}
+          checked={category === 'poisoning'}
+          type="radio"
+          name="poisoning"
+          id="poisoning"
+        />
+        <label htmlFor="poisoning">Poisoning</label>
       </div>
       <div>
-          <input type="radio" name="other" id="other" />
-          <label htmlFor="other">Other</label>
+        <input
+          onChange={({ target }) => setCategory(target.name)}
+          checked={category === 'other'}
+          type="radio"
+          name="other"
+          id="other"
+        />
+        <label htmlFor="other">Other</label>
       </div>
     </div>
   );
