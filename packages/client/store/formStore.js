efb09a3c438b6516be5905b1aecaf86ead5e787a
Rane Gray <ranegray830@gmail.com>
2023-04-06 21:42:57 +0200

    Updated formStore to reflect CategoryView form page

--- packages/client/store/formStore.js
+++ packages/client/store/formStore.js
@@ -3,27 +3,30 @@ import { create } from 'zustand';
 const initialState = {
   step: 0,
   showForm: false,
+  category: '',
   photos: [],
   title: '',
   description: '',
-  location: { lat: -8.7445, lng: 115.182 }
-}
+  location: { lat: -8.7445, lng: 115.182 },
+};
 
 const useFormStore = create((set) => ({
   ...initialState,
   step: 0,
   showForm: false,
+  category: '',
   photos: [],
   title: '',
   description: '',
   location: { lat: -8.7445, lng: 115.182 },
+  setCategory: (str) => set(() => ({ category: str })),
   setPhotos: (array) => set(() => ({ photos: array })),
   setTitle: (str) => set(() => ({ title: str })),
   setDescription: (str) => set(() => ({ description: str })),
   setLocation: (latLng) => set(() => ({ location: latLng })),
   setShowForm: (bool) => set(() => ({ showForm: bool })),
   setStep: (num) => set(() => ({ step: num })),
-  resetForm: () => set(initialState)
+  resetForm: () => set(initialState),
 }));
 
 export default useFormStore;
