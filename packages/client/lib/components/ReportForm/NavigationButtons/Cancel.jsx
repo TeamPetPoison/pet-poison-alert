0561217c9dc027469639f14b3620f917b9bf25fd
Rane Gray <ranegray830@gmail.com>
2023-04-06 22:09:07 +0200

    Added Cancel functionality to Cancel button

--- packages/client/lib/components/ReportForm/NavigationButtons/Cancel.jsx
+++ packages/client/lib/components/ReportForm/NavigationButtons/Cancel.jsx
@@ -1,14 +1,23 @@
-import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline"
+import useFormStore from '@/store/formStore';
+import { ArrowUturnLeftIcon } from '@heroicons/react/24/outline';
 
 const CancelButton = () => {
-    return (
-        <button
-            className='flex items-center justify-center w-1/2 bg-negative text-white text-3xl border py-2 px-4 rounded-2xl'
-        >
-            Cancel
-            <ArrowUturnLeftIcon className="h-8 w-8 stroke-2"/>
-        </button>
-    )
-}
+  const { resetForm, setShowForm } = useFormStore();
+
+  const handleCancel = () => {
+    resetForm();
+    setShowForm(false);
+  };
+
+  return (
+    <button
+      onClick={handleCancel}
+      className="flex items-center justify-center w-1/2 bg-negative text-white text-3xl border py-2 px-4 rounded-2xl"
+    >
+      Cancel
+      <ArrowUturnLeftIcon className="h-8 w-8 stroke-2" />
+    </button>
+  );
+};
 
 export default CancelButton;
