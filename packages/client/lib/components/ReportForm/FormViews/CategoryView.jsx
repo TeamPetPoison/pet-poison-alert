ab89a3ce77ab3ae049e08272d8c6028f00bee88e
Rane Gray <ranegray830@gmail.com>
2023-04-06 21:40:59 +0200

    Added radio buttons to CategoryView

--- packages/client/lib/components/ReportForm/FormViews/CategoryView.jsx
+++ packages/client/lib/components/ReportForm/FormViews/CategoryView.jsx
@@ -6,7 +6,34 @@ const CategoryView = () => {
   return (
     <div>
       <h2 className="text-lg mt-4">Category of incident</h2>
-        <input type="radio" name="" id="" />
+      <div>
+          <input type="radio" name="lost-pet" id="lost-pet" />
+          <label htmlFor="lost-pet">Lost/Missing Pet</label>
+      </div>
+      <div>
+          <input type="radio" name="found-pet" id="found-pet" />
+          <label htmlFor="found-pet">Found Pet</label>
+      </div>
+      <div>
+          <input type="radio" name="needs-medical" id="needs-medical" />
+          <label htmlFor="needs-medical">Needs Medical Treatment</label>
+      </div>
+      <div>
+          <input type="radio" name="gave-medical" id="gave-medical" />
+          <label htmlFor="gave-medical">Gave/Received Medical Treatment</label>
+      </div>
+      <div>
+          <input type="radio" name="gave-food" id="gave-food" />
+          <label htmlFor="gave-food">Gave Food</label>
+      </div>
+      <div>
+          <input type="radio" name="poisoning" id="poisoning" />
+          <label htmlFor="poisoning">Poisoning</label>
+      </div>
+      <div>
+          <input type="radio" name="other" id="other" />
+          <label htmlFor="other">Other</label>
+      </div>
     </div>
   );
 };
--- packages/client/lib/components/ReportForm/ReportForm.jsx
+++ packages/client/lib/components/ReportForm/ReportForm.jsx
@@ -3,7 +3,8 @@ import LocationView from './FormViews/LocationView';
 import PhotoView from './FormViews/PhotoView';
 import SummaryView from './FormViews/SummaryView';
 import FormButtons from './NavigationButtons/FormButtons';
-import ExitForm from './NavigationButtons/ExitForm'
+import CategoryView from './FormViews/CategoryView';
+import ExitForm from './NavigationButtons/ExitForm';
 import useFormStore from '@/store/formStore';
 
 const DynamicUpdateView = dynamic(() => import('./FormViews/UpdateView'), {
@@ -11,23 +12,26 @@ const DynamicUpdateView = dynamic(() => import('./FormViews/UpdateView'), {
 });
 
 const ReportForm = () => {
-  const { step } = useFormStore()
+  const { step } = useFormStore();
 
   return (
     <div className="h-screen w-full bg-white">
       {/* <ExitForm /> */}
-      {step === 3 ? null : <h1 className="text-xl font-medium">Report your incident</h1>}
-      <form action="" className='flex flex-col'>
-        {step === 0 ? <PhotoView /> : null}
-        {step === 1 ? <SummaryView /> : null}
-        {step === 2 ? <LocationView /> : null}
-        {step === 3 ? <DynamicUpdateView /> : null}
-        <div className='flex-1'>
-          <div className='flex mt-80'>
+      {step === 3 ? null : (
+        <h1 className="text-xl font-medium">Report your incident</h1>
+      )}
+      <form action="" className="flex flex-col">
+        {step === 0 ? <CategoryView /> : null}
+        {step === 1 ? <PhotoView /> : null}
+        {step === 2 ? <SummaryView /> : null}
+        {step === 3 ? <LocationView /> : null}
+        {step === 4 ? <DynamicUpdateView /> : null}
+        <div className="flex-1">
+          <div className="flex mt-80">
             <p>Do not enter </p>
           </div>
         </div>
-        {step === 3 ? null : <FormButtons />}
+        {step === 4 ? null : <FormButtons />}
       </form>
     </div>
   );
