697cb55002c3644d4c18eaadb3bd8fe217d93202
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-05-06 19:45:25 +0800

    LocationSearch

--- packages/client/lib/components/SearchBar.jsx
+++ packages/client/lib/components/SearchBar.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import React, { useState,useRef } from 'react';
 import { Bars3Icon, MagnifyingGlassCircleIcon, ArrowUturnLeftIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import useOnClickOutside from './useOnClickOutside';
+
 
 const mockSearchLocationData = {
   results: [
@@ -26,79 +28,101 @@ const mockSearchLocationData = {
   ],
 };
 
-function getSearchLocationData(searchText) {
-  const results = mockSearchLocationData.results.filter((result) => {
-    return result.name.toLowerCase().includes(searchText.toLowerCase());
-  });
-  return results;
-}
-
-function LocationSearch() {
+function SearchBar() {
+  const [isOpen, setIsOpen] = useState(true);
   const [value, setValue] = useState('');
   const [results, setResults] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
+  const searchRef= useRef(null);
 
-  function handleInputChange(event) {
-    const value = event.target.value;
-    setValue(value);
-    const searchResults = getSearchLocationData(value);
-    setResults(searchResults);
-    setIsOpen(searchResults.length > 0);
-  }
-
-  function handleClearInput() {
-    setValue('');
-  }
 
   function handleToggleSearch() {
     setIsOpen(!isOpen);
   }
 
-  function handleSearch() {
-    console.log('search', value);
+  function getSearchLocationData(searchText) {
+    const results = mockSearchLocationData.results.filter((result) => {
+      return result.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    return results;
+  }
+
+  function handleInputChange(event) {
+    const value = event.target.value;
+    setValue(value);
+    if(value===""){
+      setIsOpen(false);
+      setResults([]);
+    }
+    else{
+    const searchResults = getSearchLocationData(value);
+    setResults(searchResults);
+    setIsOpen(searchResults.length > 0);
+  }
+    
+  }
+
+  function handleClearInput() {
+    setValue('');
+    setResults([]);
+    setIsOpen(false);
+    
+  }
+
+  function handleListItemClick(name) {
+    setValue(name);
+    setIsOpen(false);
+    
+  }
+
+  function hideSearch() {
     setIsOpen(false);
   }
+
+useOnClickOutside(searchRef,hideSearch);
 
   return (
     <div className="fixed top-8 left-6 right-6 z-[9999] bg-background text-foreground rounded-2xl">
       <div className="relative">
         <div className='flex px-5 py-3 justify-between items-center gap-x-4 border border-solid border-white-500 rounded-lg'>
-
-          {isOpen ? (
-            <ArrowUturnLeftIcon className='w-6 h-6' onClick={handleToggleSearch} />
-          ) : (
-            <Bars3Icon className='w-6 h-6' onClick={handleToggleSearch} />
-          )}
-
+          <button onClick={handleToggleSearch}>
+            {isOpen ? (
+              <ArrowUturnLeftIcon className='w-6 h-6'/>
+            ) : (
+              <Bars3Icon className='w-6 h-6'/>
+            )}
+          </button>
+   
           <input
+            ref={searchRef}
             className="w-full pl-2 placeholder:text-foreground/30 border border-solid border-white-500 rounded-lg"
             type="text"
             placeholder='Search For Place, Location'
             value={value}
             onChange={handleInputChange}
           />
-
-          {value && (
-            <XMarkIcon className='w-6 h-6' onClick={handleClearInput} />
-          )}
-
-          <MagnifyingGlassCircleIcon className='w-6 h-6' onClick={handleSearch} />
+          <button 
+          onClick={handleClearInput}>
+            {value && (
+              <MagnifyingGlassCircleIcon className='w-6 h-6' />
+            )}
+             <XMarkIcon className='w-6 h-6' />
+            
+          </button>
         </div>
-
+   
         {isOpen && (
           <div className="absolute top-14 left-0 right-0 bg-white border border-solid border-white-500 rounded-b-lg">
-            {results.slice(0, 10).map((item) => (
-              <div
-                key={item.name}
-                className="px-5 py-2 hover:bg-purple-200 cursor-pointer"
-                onClick={() => {
-                  setValue(item.name);
-                  handleSearch();
-                }}
-              >
-                {item.name}
-              </div>
-            ))}
+            <ul className='list-none p-0'>
+              {results.slice(0, 10).map((item) => (
+                <li
+                  key={item.name}
+                  className="px-5 py-2 hover:bg-selected-10 cursor-pointer"
+                  onClick={() => handleListItemClick(item.name)}
+                >
+                  <button className='w-full h-full text-left'>  {item.name} </button>
+                </li>
+              ))}
+            </ul>
           </div>
         )}
       </div>
@@ -106,4 +130,7 @@ function LocationSearch() {
   );
 }
 
-export default LocationSearch;
+export default SearchBar;
+
+
+
---
+++ packages/client/lib/components/useOnClickOutside.js
@@ -0,0 +1,26 @@
+import  {useState,useEffect} from "react";
+
+function useOnClickOutside(ref,handler){
+
+const[isMounted,setIsMounted]=useState(false);
+
+useEffect(()=>{
+setIsMounted(true);
+
+function handleClickOutside(event){
+if(isMounted && ref.current && !ref.current.contains(event.target)){
+    handler()
+}
+}
+document.addEventListener('mousedown',handleClickOutside);
+return ()=> {
+    setIsMounted(false)
+
+    document.removeEventListener('mousedown',handleClickOutside);
+};
+}, [ref,handler,isMounted]);
+}
+
+export default useOnClickOutside;
+
+
