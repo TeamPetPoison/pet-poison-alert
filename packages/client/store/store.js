ad361a84eea02fed735df2712b4452b1bebfd54c
Rane Gray <ranegray830@gmail.com>
2023-05-26 14:01:56 +0200

    fixed faulty logic in setGeoData store

--- packages/client/store/store.js
+++ packages/client/store/store.js
@@ -4,7 +4,7 @@ const useStore = create(set => ({
     geoData: {lat: -8.7445, lng: 115.182},
     markers: [],
     setMarkers: (object) => set((state) => ({markers: [...state.markers, object]})),
-    setGeoData: (location) => set(state => state.geoData = location)
+    setGeoData: (location) => set({geoData: location})
 }))
 
 export default useStore;
