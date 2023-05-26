233ed0ceaa7210066e568ea9caf7c2c18f8aacad
Rane Gray <ranegray830@gmail.com>
2023-05-26 14:04:57 +0200

    sorted formStore state A-Z

--- packages/client/store/formStore.js
+++ packages/client/store/formStore.js
@@ -13,23 +13,23 @@ const initialState = {
 
 const useFormStore = create((set) => ({
   ...initialState,
-  step: 0,
-  error: false,
-  showForm: false,
   category: '',
-  photos: [],
-  title: '',
   description: '',
+  error: false,
   location: { lat: -8.7445, lng: 115.182 },
+  photos: [],
+  resetForm: () => set(initialState),
   setCategory: (str) => set(() => ({ category: str })),
-  setPhotos: (array) => set(() => ({ photos: array })),
-  setTitle: (str) => set(() => ({ title: str })),
   setDescription: (str) => set(() => ({ description: str })),
+  setError: (bool) => set(() => ({ error: bool })),
   setLocation: (latLng) => set(() => ({ location: latLng })),
+  setPhotos: (array) => set(() => ({ photos: array })),
   setShowForm: (bool) => set(() => ({ showForm: bool })),
   setStep: (num) => set(() => ({ step: num })),
-  setError: (bool) => set(() => ({ error: bool })),
-  resetForm: () => set(initialState),
+  setTitle: (str) => set(() => ({ title: str })),
+  showForm: false,
+  step: 0,
+  title: '',
 }));
 
 export default useFormStore;
