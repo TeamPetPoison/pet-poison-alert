9718e560b6755e7b3a2f1e24ea8b073a443339a9
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-09-22 15:40:32 +0800

    separate actions and state in main store

--- packages/client/store/store.js
+++ packages/client/store/store.js
@@ -1,15 +1,32 @@
 //@ts-check
 import { create } from 'zustand';
 
-const useMainStore = create((set) => ({
+/**
+ * main global zustand store
+ * - stores the state
+ * - actions separated into the `useMainStoreActions` object
+ */
+const useMainStore = create(() => ({
   geoData: { lat: -8.7445, lng: 115.182 },
   markers: [],
-  setMarkers: (object) =>
-    set((state) => ({ markers: [...state.markers, object] })),
+  rootDimensions: { width: 0, height: 0 },
+  windowDimensions: { width: 0, height: 0 },
+}));
+
+/**
+ * state updater funcs for the main zustand store
+ */
+export const useMainStoreActions = () => ({
   setGeoData: ({ lat, lng }) => {
     console.info('setGeoData', lat, lng);
-    set({ geoData: { lat, lng } });
+    useMainStore.setState({ geoData: { lat, lng } });
   },
-}));
+  setMarkers: (object) =>
+    useMainStore.setState((state) => ({ markers: [...state.markers, object] })),
+  setRootDimensions: ({ width, height }) =>
+    useMainStore.setState({ rootDimensions: { width, height } }),
+  setWindowDimensions: ({ width, height }) =>
+    useMainStore.setState({ windowDimensions: { width, height } }),
+});
 
 export default useMainStore;
