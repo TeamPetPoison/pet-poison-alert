99e7167089783e01bfe946e75bf97b83b736fd37
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-09-24 14:18:09 +0800

    update css in summary to fit mobile view better

--- packages/client/lib/components/ReportForm/FormViews/SummaryView.jsx
+++ packages/client/lib/components/ReportForm/FormViews/SummaryView.jsx
@@ -12,7 +12,7 @@ const SummaryView = () => {
     <div className="flex-1">
       <h2 className="mt-4 text-xl">Summary of report</h2>
       <h3 className="text-md">Please confirm the details of your report</h3>
-      <h2 className="mt-4 text-xl">Category</h2>
+      <h3 className="mt-4 text-xl">Category</h3>
       <div className="flex items-center">
         <input type="radio" readOnly checked className="h-4 w-4" />
         <label htmlFor="" className="pl-3">
@@ -20,14 +20,14 @@ const SummaryView = () => {
         </label>
       </div>
       <h2 className="mt-4 text-xl">Photos</h2>
-      <div className="flex h-[100px]">
+      <div className="flex flex-row flex-wrap">
         {photos.length > 0 ? (
           photos.map((photo, index) => (
             <div key={index} className="relative m-1">
               <Image
-                width={20}
-                height={20}
-                className="h-auto w-20 rounded-lg"
+                width={80}
+                height={80}
+                className="w-20 rounded-lg"
                 src={URL.createObjectURL(photo)}
                 onLoad={(e) => {
                   if ('src' in e.target && typeof e.target.src === 'string') {
@@ -53,10 +53,11 @@ const SummaryView = () => {
       <h2 className="mt-4 text-xl">Description</h2>
       <textarea
         value={description}
-        rows={4}
-        className="text-foreground bg-background border-border focus:ring-primary focus:primary block w-full rounded-lg border p-2.5 shadow-md"
+        rows={2}
+        className="text-foreground bg-background border-border focus:ring-primary focus:primary block w-full resize-y rounded-lg border p-2.5 shadow-md"
         placeholder="Provide a description of your report"
         disabled
+        // allow resizing of textarea
       ></textarea>
       <h2 className="mt-4 text-xl">Location</h2>
       <input
