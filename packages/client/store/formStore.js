7f8b202ca7dcd6ab47d7d9b3238eaf08f0875eb6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-09-30 22:59:25 +0800

    separate namespace for useFormStore

--- packages/client/store/formStore.js
+++ packages/client/store/formStore.js
@@ -11,36 +11,41 @@ const initialState = {
   title: '',
 };
 
-const useFormStore = create((set, get) => ({
+export const useFormStore = create((set, get) => ({
   ...initialState,
   category: '',
   description: '',
   error: false,
   location: { lat: -8.7445, lng: 115.182 },
   photos: [],
-  resetForm: () => set(initialState),
-  getFormData: () => {
-    const { category, description, location, photos, title } = get();
-    return {
-      id: Date.now(),
-      category: category,
-      photos: photos,
-      title: title,
-      description: description,
-      location: location,
-    };
-  },
-  setCategory: (str) => set(() => ({ category: str })),
-  setDescription: (str) => set(() => ({ description: str })),
-  setError: (bool) => set(() => ({ error: bool })),
-  setLocation: (latLng) => set(() => ({ location: latLng })),
-  setPhotos: (array) => set(() => ({ photos: array })),
-  setShowForm: (bool) => set(() => ({ showForm: bool })),
-  setStep: (num) => set(() => ({ step: num })),
-  setTitle: (str) => set(() => ({ title: str })),
   showForm: false,
   step: 0,
   title: '',
+  actions: {
+    resetForm: () => set(initialState),
+    getFormData: () => {
+      const { category, description, location, photos, title } = get();
+      return {
+        id: Date.now(),
+        category: category,
+        photos: photos,
+        title: title,
+        description: description,
+        location: location,
+      };
+    },
+    setCategory: (str) => set({ category: str }),
+    setDescription: (str) => set({ description: str }),
+    setError: (bool) => set({ error: bool }),
+    setLocation: (latLng) => set({ location: latLng }),
+    setPhotos: (array) => set({ photos: array }),
+    setShowForm: (bool) => set({ showForm: bool }),
+    setStep: (num) => set({ step: num }),
+    setTitle: (str) => set({ title: str }),
+  },
 }));
 
-export default useFormStore;
+/**
+ * state updater funcs for the main zustand store
+ */
+export const useFormStoreActions = () => useFormStore((state) => state.actions);
