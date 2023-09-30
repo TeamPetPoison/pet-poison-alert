1396c39c8631d36a3a1237f87b1a91db4733d0f6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-09-30 22:59:53 +0800

    normalization helper to round lat, lng

---
+++ packages/client/lib/helpers/helpers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {{lat: number, lng: number}} args
+ * @returns {{lat: number, lng: number}} normalized lat lng
+ */
+export const normalizeLatLng = ({ lat, lng }) => {
+  if (typeof lat !== 'number' || typeof lng !== 'number') {
+    throw new Error('lat and lng must be numbers');
+  }
+  // lat and lng should be truncated to 6 decimal places
+  const truncLat = parseFloat(lat.toFixed(6));
+  const truncLng = parseFloat(lng.toFixed(6));
+  return { lat: truncLat, lng: truncLng };
+};
